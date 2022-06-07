57d93626f3023a20b489a471755354725035b720
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-08 06:57:57 +0700

    Add custom view to book seats.

---
+++ app/src/main/java/com/taufik/androidintemediate/customviewfromscratch/SeatsActivity.kt
@@ -0,0 +1,32 @@
+package com.taufik.androidintemediate.customviewfromscratch
+
+import android.os.Bundle
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidintemediate.databinding.ActivitySeatsBinding
+
+class SeatsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySeatsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySeatsBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        setContentView(binding.root)
+
+        setActionClick()
+    }
+
+    private fun setActionClick() = with(binding) {
+        btnFinish.setOnClickListener {
+            seatsView.seat?.let {
+                Toast.makeText(this@SeatsActivity, "Kursi Anda nomor ${it.name}", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this@SeatsActivity, "Silakan pilih kursi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
