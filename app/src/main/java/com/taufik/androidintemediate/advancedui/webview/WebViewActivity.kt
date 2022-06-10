ef1cd9dab76681231c17bdcab908826921cd38c6
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-11 06:37:32 +0700

    Add webview.

---
+++ app/src/main/java/com/taufik/androidintemediate/advancedui/webview/WebViewActivity.kt
@@ -0,0 +1,42 @@
+package com.taufik.androidintemediate.advancedui.webview
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidintemediate.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initWebView()
+    }
+
+    private fun initWebView() = with(binding) {
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView, url: String, message: String, result: android.webkit.JsResult): Boolean {
+                Toast.makeText(this@WebViewActivity, message, Toast.LENGTH_LONG).show()
+                result.confirm()
+                return true
+            }
+        }
+
+        webView.loadUrl("https://www.dicoding.com")
+    }
+}
