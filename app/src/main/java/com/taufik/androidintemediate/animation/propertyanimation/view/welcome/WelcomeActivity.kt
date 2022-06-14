64a5e5f0a26e6a03d0d3de959c991207fb684197
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-14 16:42:27 +0700

    Add logic to welcome activity class.

--- app/src/main/java/com/taufik/androidintemediate/animation/propertyanimation/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/taufik/androidintemediate/animation/propertyanimation/view/welcome/WelcomeActivity.kt
@@ -1,7 +1,13 @@
 package com.taufik.androidintemediate.animation.propertyanimation.view.welcome
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidintemediate.animation.propertyanimation.view.login.LoginActivity
+import com.taufik.androidintemediate.animation.propertyanimation.view.signup.SignUpActivity
 import com.taufik.androidintemediate.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
@@ -12,5 +18,31 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() = with(binding) {
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this@WelcomeActivity, LoginActivity::class.java))
+        }
+
+        btnSignup.setOnClickListener {
+            startActivity(Intent(this@WelcomeActivity, SignUpActivity::class.java))
+        }
     }
 }
