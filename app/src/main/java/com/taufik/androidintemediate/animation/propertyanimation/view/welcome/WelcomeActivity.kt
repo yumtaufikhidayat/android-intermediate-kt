27f4dda7fc35a37328b84f92bc878416de38be36
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-14 17:02:11 +0700

    Add animation to welcome activity.

--- app/src/main/java/com/taufik/androidintemediate/animation/propertyanimation/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/taufik/androidintemediate/animation/propertyanimation/view/welcome/WelcomeActivity.kt
@@ -1,8 +1,11 @@
 package com.taufik.androidintemediate.animation.propertyanimation.view.welcome
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -21,6 +24,7 @@ class WelcomeActivity : AppCompatActivity() {
 
         setupView()
         setupAction()
+        playAnimation()
     }
 
     private fun setupView() {
@@ -43,6 +47,39 @@ class WelcomeActivity : AppCompatActivity() {
 
         btnSignup.setOnClickListener {
             startActivity(Intent(this@WelcomeActivity, SignUpActivity::class.java))
+        }
+    }
+
+    private fun playAnimation() = with(binding) {
+        /*
+            objek gambar
+            bergerak secara horizontal
+            dengan durasi 6 detik
+            bergerak sejauh 60f (-30f s.d. 30f)
+            kembali ke titik semula (repeatMode = reverse)
+            animasi terus berjalan (repeatCount = infinity)
+        */
+        ObjectAnimator.ofFloat(imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        // Menambahkan animasi untuk masing-masing view
+        val login = ObjectAnimator.ofFloat(btnLogin, View.ALPHA, 1f).setDuration(500)
+        val signup = ObjectAnimator.ofFloat(btnSignup, View.ALPHA, 1f).setDuration(500)
+        val title = ObjectAnimator.ofFloat(tvTitle, View.ALPHA, 1f).setDuration(500)
+        val desc = ObjectAnimator.ofFloat(tvDesc, View.ALPHA, 1f).setDuration(500)
+
+        // dua button bergerak secara bersamaan
+        val together = AnimatorSet().apply {
+            playTogether(login, signup)
+        }
+
+        // tiga TextView lainnya bergerak secara sekuensial
+        AnimatorSet().apply {
+            playSequentially(title, desc, together)
+            start()
         }
     }
 }
