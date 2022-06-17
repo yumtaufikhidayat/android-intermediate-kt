2e81318a874ae92abe08637600a9b8e90b20ae65
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-17 07:36:19 +0700

    Add motion layout.

---
+++ app/src/main/java/com/taufik/androidintemediate/animation/motionlayout/MotionMainActivity.kt
@@ -0,0 +1,16 @@
+package com.taufik.androidintemediate.animation.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidintemediate.databinding.ActivityMotionMainBinding
+
+class MotionMainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMotionMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMotionMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
