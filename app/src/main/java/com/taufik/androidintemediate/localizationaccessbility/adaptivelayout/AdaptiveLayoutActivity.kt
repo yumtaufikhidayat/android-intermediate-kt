da225ce81138ec091e94466b6b4481cd594d28a2
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-19 19:03:50 +0700

    Add adaptive layout.

---
+++ app/src/main/java/com/taufik/androidintemediate/localizationaccessbility/adaptivelayout/AdaptiveLayoutActivity.kt
@@ -0,0 +1,27 @@
+package com.taufik.androidintemediate.localizationaccessbility.adaptivelayout
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidintemediate.databinding.ActivityAdaptiveLayoutBinding
+
+class AdaptiveLayoutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdaptiveLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAdaptiveLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        setAction()
+    }
+
+    private fun setAction() = with(binding) {
+        imgSetting.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+    }
+}
