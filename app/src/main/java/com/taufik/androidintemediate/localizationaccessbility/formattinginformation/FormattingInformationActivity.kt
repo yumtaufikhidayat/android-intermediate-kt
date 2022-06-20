b555c6150bb013a6473b24d6639ae1029d3b5a66
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-20 15:51:49 +0700

    Add formatting information.

---
+++ app/src/main/java/com/taufik/androidintemediate/localizationaccessbility/formattinginformation/FormattingInformationActivity.kt
@@ -0,0 +1,63 @@
+package com.taufik.androidintemediate.localizationaccessbility.formattinginformation
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidintemediate.R
+import com.taufik.androidintemediate.databinding.ActivityFormattingInformationBinding
+import com.taufik.androidintemediate.localizationaccessbility.formattinginformation.Helper.withCurrencyFormat
+import com.taufik.androidintemediate.localizationaccessbility.formattinginformation.Helper.withDateFormat
+import com.taufik.androidintemediate.localizationaccessbility.formattinginformation.Helper.withNumberingFormat
+
+class FormattingInformationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormattingInformationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFormattingInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+        setupData()
+    }
+
+    private fun setupAction() {
+        binding.imgSetting.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupData() = with(binding) {
+        val repository = RemoteDataSource(this@FormattingInformationActivity)
+        val product = repository.getDetailProduct().apply {
+            imgPreview.setImageResource(image)
+            tvName.text = name
+            tvStore.text = store
+            tvColor.text = color
+            tvSize.text = size
+            tvDesc.text = desc
+            tvPrice.text = price.withCurrencyFormat()
+            tvDate.text = getString(R.string.dateFormat, date.withDateFormat())
+            tvRating.text = getString(R.string.ratingFormat, date.withNumberingFormat(), countRating.withNumberingFormat())
+        }
+    }
+}
---
+++ app/src/main/java/com/taufik/androidintemediate/localizationaccessbility/formattinginformation/RemoteDataSource.kt
@@ -0,0 +1,21 @@
+package com.taufik.androidintemediate.localizationaccessbility.formattinginformation
+
+import android.content.Context
+import com.taufik.androidintemediate.R
+
+class RemoteDataSource(private val context: Context) {
+    fun getDetailProduct(): ProductModel {
+        return ProductModel(
+            name = context.getString(R.string.product_name),
+            store = context.getString(R.string.product_store),
+            size = context.getString(R.string.product_size),
+            color = context.getString(R.string.product_color),
+            desc = context.getString(R.string.product_description),
+            image = R.drawable.shoes,
+            date = context.getString(R.string.product_date),
+            rating = context.getString(R.string.product_rating),
+            price = context.getString(R.string.product_price),
+            countRating = context.getString(R.string.product_count_rating)
+        )
+    }
+}
