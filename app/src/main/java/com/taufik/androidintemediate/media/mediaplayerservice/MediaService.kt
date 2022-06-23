fc567e9a06efd6278d438fcdf5f416f503e7442f
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-23 11:18:00 +0700

    Add media player service notification.

--- app/src/main/java/com/taufik/androidintemediate/media/mediaplayerservice/MediaService.kt
+++ app/src/main/java/com/taufik/androidintemediate/media/mediaplayerservice/MediaService.kt
@@ -1,11 +1,16 @@
 package com.taufik.androidintemediate.media.mediaplayerservice
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.*
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.taufik.androidintemediate.R
 import java.io.IOException
 import java.lang.ref.WeakReference
@@ -76,6 +81,7 @@ class MediaService: Service(), MediaPlayerCallback {
             setOnPreparedListener {
                 isReady = true
                 mMediaPlayer?.start()
+                showNotification()
             }
 
             setOnErrorListener { _, _, _ -> false }
@@ -90,6 +96,7 @@ class MediaService: Service(), MediaPlayerCallback {
                 mMediaPlayer?.pause()
             } else {
                 mMediaPlayer?.start()
+                showNotification()
             }
         }
     }
@@ -98,6 +105,7 @@ class MediaService: Service(), MediaPlayerCallback {
         if (mMediaPlayer?.isPlaying as Boolean || isReady) {
             mMediaPlayer?.stop()
             isReady = false
+            stopNotification()
         }
     }
 
@@ -116,11 +124,53 @@ class MediaService: Service(), MediaPlayerCallback {
         }
     }
 
+    private fun showNotification() {
+        val notificationIntent = Intent(this, ServiceMediaPlayerActivity::class.java)
+        notificationIntent.flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            PendingIntent.getActivity(this, NOTIFICATION_REQUEST_CODE, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getActivity(this, NOTIFICATION_REQUEST_CODE, notificationIntent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
+            .setContentTitle("TES1")
+            .setContentText("TES2")
+            .setSmallIcon(R.drawable.ic_dicoding)
+            .setContentIntent(pendingIntent)
+            .setTicker("TES3")
+            .build()
+
+        createChannel(CHANNEL_DEFAULT_IMPORTANCE)
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
+    }
+
+    private fun createChannel(channelId: String) {
+
+        val mNotificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Battery", NotificationManager.IMPORTANCE_DEFAULT)
+            channel.apply {
+                setShowBadge(false)
+                setSound(null, null)
+            }
+            mNotificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun stopNotification() {
+        stopForeground(false)
+    }
+
     companion object {
         const val ACTION_CREATE = "com.taufik.androidintemediate.media.mediaplayerservice.ACTION_CREATE"
         const val ACTION_DESTROY = "com.taufik.androidintemediate.media.mediaplayerservice.ACTION_DESTROY"
         const val TAG = "MediaService"
         const val PLAY = 0
         const val STOP = 1
+        const val CHANNEL_DEFAULT_IMPORTANCE = "com.taufik.androidintemediate.media.mediaplayerservice.CHANNEL_TEST"
+        const val ONGOING_NOTIFICATION_ID = 1
+        const val NOTIFICATION_REQUEST_CODE = 0
     }
 }
