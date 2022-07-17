448f3d1e8f089f2360bbde95ba3ebfccf15cad58
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2022-07-18 05:56:32 +0700

    Add invalid ISO and invalid timezone.

--- app/src/test/java/com/taufik/androidintemediate/advancedtesting/utils/DateFormatterTest.kt
+++ app/src/test/java/com/taufik/androidintemediate/advancedtesting/utils/DateFormatterTest.kt
@@ -2,6 +2,8 @@ package com.taufik.androidintemediate.advancedtesting.utils
 
 import org.junit.Assert
 import org.junit.Test
+import java.time.format.DateTimeParseException
+import java.time.zone.ZoneRulesException
 
 class DateFormatterTest {
     @Test
@@ -10,5 +12,21 @@ class DateFormatterTest {
         Assert.assertEquals("02 Feb 2022 | 17:10", DateFormatter.formatDate(currentDate, "Asia/Jakarta"))
         Assert.assertEquals("02 Feb 2022 | 18:10", DateFormatter.formatDate(currentDate, "Asia/Makassar"))
         Assert.assertEquals("02 Feb 2022 | 19:10", DateFormatter.formatDate(currentDate, "Asia/Jayapura"))
+    }
+
+    @Test
+    fun `given wrong ISO 8601 format then should throw error`() {
+        val wrongFormat = "2022-02-02T10:10"
+        Assert.assertThrows(DateTimeParseException::class.java) {
+            DateFormatter.formatDate(wrongFormat, "Asia/Jakarta")
+        }
+    }
+
+    @Test
+    fun `given invalid timezone then should throw error`() {
+        val wrongFormat = "2022-02-02T10:10:10Z"
+        Assert.assertThrows(ZoneRulesException::class.java) {
+            DateFormatter.formatDate(wrongFormat, "Asia/Bandung")
+        }
     }
 }
